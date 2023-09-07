ee9940b980d6357209d87bed26fa778a73f9e359
Eric Graves <eridgraves@gmail.com>
2023-09-06 21:52:10 -0500

    Adding barebones proj, no RPC or protobuf compiler

---
+++ WateringServer/WateringServer.cs
@@ -0,0 +1,16 @@
+﻿// WateringServer
+// Backend server. Controller for WateringNodes, should run on an x86 based machine with wifi or BLE support. Each Node is an Arduino or ESP32 controlling one or more pumps that water plants.
+// The controller provides RPC over wifi or BLE-based control and monitoring of the nodes. On BLE master can control up to 20 slave devices.
+
+using System;
+
+namespace WateringServer 
+{
+    internal class WateringMonitor
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
